a4cb9abda9df742067cb437b890b25b9b74b26eb
brunoflma <brunoferreiram@outlook.com>
2022-08-22 18:55:31 -0300

    Script Automatização Apache

---
+++ apache_iac/apache_iac.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Atualizando o servidor... Aguarde"
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+
+echo "Baixando e copiando os arquivos necessários... Aguarde"
+
+cd /tmp
+wget https://github.com/brunoflma/diolinuxexp/apache_iac/main.zip
+unzip main.zip
+cd main
+cp -R * /var/www/html/
